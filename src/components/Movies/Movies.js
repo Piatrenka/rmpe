2d3b2a7e13fb1f3d27a152ebd15665229e2f5647
Ihar Piatrenka <iharpiatrenka@Ihars-iMac.local>
2020-02-12 15:03:38 +0200

    Split movieinfo comp in two

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./Movies.css";
 
-import MovieInfo from "../MovieInfo/MovieInfo";
+import MovieInfoShort from "../MovieInfoShort/MovieInfoShort";
 
-import { Container, Row, Col, Card } from "react-bootstrap"
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 // import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -14,10 +14,10 @@ const Movies = props => {
     return (
       <Col xs="4" className="p-2" style={{ backgroundColor: "grey" }}>
         <Card key={movie.imdb.id} style={{ backgroundColor: "#0A0B0D" }}>
-          <MovieInfo
+          <MovieInfoShort
             key={movie.imdb.id}
             movie={movie}
-            short={true}
+            // short={true}
             onMovieClick={props.onMovieClick}
           />
         </Card>
@@ -28,9 +28,7 @@ const Movies = props => {
   return (
     <div>
       {/* <h6>This is Movies Component</h6> */}
-      <Row style={{margin: "0px"}}> 
-        {movies}
-      </Row>
+      <Row style={{ margin: "0px" }}>{movies}</Row>
     </div>
   );
 };
