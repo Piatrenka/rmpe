3e1e817f2ab0eb49e5cc63983d64b5e77ce195f3
Ihar Piatrenka <piatrenka@gmail.com>
2020-02-22 18:56:55 +0300

    Add more tests

--- 04-testing/src/common/sync.spec.js
+++ 04-testing/src/common/sync.spec.js
@@ -3,13 +3,18 @@ const Lodash = require('./sync')
 // tdd 
 // в начале тест, потом реализация, в итоге стопроцентное покрытие тестами
 // подход медленный потому что неэффективный
+
+
+let _ = new Lodash()
+
 describe('Lodash: compact', () => {
-  let _ = new Lodash()
   let arr
+  let res
 
   // hook и он будет вызываться перед каждым тестом
   beforeEach(() => {
     arr = [false, 42, 0, '', true, null, 'hi'] 
+    res = [42, true, "hi"];
   })
 
   // еще варианты
@@ -22,9 +27,17 @@ describe('Lodash: compact', () => {
     expect(_.compact).not.toBeUndefined()
   })
 
+  // отдельная инициализация перед каждым тестом позволяет писать тесты, которые могут модифицировать тестовые данные
+  test('should working array be editable', () => {
+    // способ добавления элементов в массив с помощью другого массива
+    arr.push(...['lo', 'by'])
+    expect(_.compact(arr)).toContain('lo')
+    expect(_.compact(arr)).toContain('by')
+  })
+
   test('should remove falsy values from array', () => {
-    const res = [42, true, 'hi']
-    // expect(_.compact(array)).toBe(res)
+    // const res = [42, true, 'hi']
+    // expect(_.compact(array)).toBe(res)   // Такой вариант не прокатывает в случае массива
     expect(_.compact(arr)).toEqual(res)
   })
   
@@ -36,4 +49,33 @@ describe('Lodash: compact', () => {
     expect(_.compact(arr)).not.toContain('')
     expect(_.compact(arr)).not.toContain(null)
   })
+})
+
+describe('Lodash: groubBy', () => {
+  test('should be defiend', () => {
+    expect(_.groupBy).toBeDefined()
+  })
+
+  test('should group array items by Math.floor', () => {
+    const arr = [2.2, 2.4, 4.2, 3.1]
+    const res = {
+      2: [2.2, 2.4],
+      4: [4.2],
+      3: [3.1]
+    }
+    expect(_.groupBy(arr, Math.floor)).toEqual(res)
+  })
+
+  test('should group array items by length', () => {
+    const arr = ['one', 'three', 'two']
+    const res = {
+      5: ['three'],
+      3: ['one', 'two']
+    }
+    expect(_.groupBy(arr, 'length')).toEqual(res)
+  })
+
+  test('should NOT return array', () => {
+    expect(_.groupBy([], Math.trunc)).not.toBeInstanceOf(Array)
+  })  
 })
