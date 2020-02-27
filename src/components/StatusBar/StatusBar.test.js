292b9b0a1db2f849b73e3602ed717c1fa1015158
Ihar Piatrenka <iharpiatrenka@Ihars-iMac.local>
2020-02-27 15:05:21 +0200

    Add test for statusbar

--- src/components/StatusBar/StatusBar.test.js
+++ src/components/StatusBar/StatusBar.test.js
@@ -1,14 +1,28 @@
 import "@testing-library/jest-dom";
 import React from "react";
 
-import { render, fireEvent, screen } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  screen,
+  waitForElement
+} from "@testing-library/react";
 import StatusBar from "./StatusBar";
 
 describe("<StatusBar />", () => {
-  it("Should render the amount of films", () => {
+  it("Should render the amount of films in plural case", async () => {
     const moviesAmount = 42;
-    render(<StatusBar detectedAmount={moviesAmount} />);
+    const { getByText } = render(<StatusBar detectedAmount={moviesAmount} />);
 
-    expect(screen.queryByText(/^zz/i)).toBeInTheDocument;
+    // expect(screen.queryByText(/^zz/i)).toBeInTheDocument;
+    await waitForElement(() => getByText(/42 movies was found/i));
+  });
+
+  it("Should render the amount of films in single case", async () => {
+    const moviesAmount = 1;
+    const { getByText } = render(<StatusBar detectedAmount={moviesAmount} />);
+
+    // expect(screen.queryByText(/^zz/i)).toBeInTheDocument;
+    await waitForElement(() => getByText(/1 movie was found/i));
   });
 });
