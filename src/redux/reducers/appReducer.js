01613b400d4ca0994a79001a02293b616791e2a1
Ihar Piatrenka <piatrenka@gmail.com>
2020-04-21 17:51:25 +0300

    Disable absolete reducer states and cases

--- src/redux/reducers/appReducer.js
+++ src/redux/reducers/appReducer.js
@@ -3,20 +3,20 @@ import {
   FETCH_MOVIES_START, 
   FETCH_MOVIES_ERR, 
   FETCH_MOVIES_SUCCESS, 
-  UPDATE_SEARCH_QUERY, 
-  SET_SEARCHBY_MODE, 
-  SET_SORTBY_MODE, 
-  MOVIE_CLICK, 
-  RETURN_2_SEARCH 
+  // UPDATE_SEARCH_QUERY, 
+  // SET_SEARCHBY_MODE, 
+  // SET_SORTBY_MODE, 
+  // MOVIE_CLICK, 
+  // RETURN_2_SEARCH 
 } from '../actions/actionTypes'
 
 const initialState = {
   movies: [],
-  recordsTotal: 0,
-  searchQuery: "", // Строка поиска
-  searchBy: SearchModes.TITLE, // режим поиска
-  sortBy: SortModes.RATING,  // режим сортировки
-  selectedMovieId: null,
+  // recordsTotal: 0,
+  // searchQuery: "", // Строка поиска
+  // searchBy: SearchModes.TITLE, // режим поиска
+  // sortBy: SortModes.RATING,  // режим сортировки
+  // selectedMovieId: null,
   loading: true
 }
 
@@ -34,27 +34,27 @@ export default function appReducer(state = initialState, action) {
       return {
         ...state, loading: false, error: action.error
       }
-    case UPDATE_SEARCH_QUERY:
-      return {
-        ...state, searchQuery: action.searchQuery
-      }  
-    case SET_SEARCHBY_MODE:
-      return {
-        ...state, searchBy: action.searchBy
-      }  
-    case SET_SORTBY_MODE:
-      const movies = reorderMovies(state.movies, action.sortBy)
-      return {
-        ...state, sortBy: action.sortBy, movies: movies
-      }
-    case MOVIE_CLICK:
-      return {
-        ...state, selectedMovieId: action.id
-      }    
-    case RETURN_2_SEARCH:
-      return {
-        ...state, selectedMovieId: null
-      }  
+    // case UPDATE_SEARCH_QUERY:
+    //   return {
+    //     ...state, searchQuery: action.searchQuery
+    //   }  
+    // case SET_SEARCHBY_MODE:
+    //   return {
+    //     ...state, searchBy: action.searchBy
+    //   }  
+    // case SET_SORTBY_MODE:
+    //   const movies = reorderMovies(state.movies, action.sortBy)
+    //   return {
+    //     ...state, sortBy: action.sortBy, movies: movies
+    //   }
+    // case MOVIE_CLICK:
+    //   return {
+    //     ...state, selectedMovieId: action.id
+    //   }    
+    // case RETURN_2_SEARCH:
+    //   return {
+    //     ...state, selectedMovieId: null
+    //   }  
     default:
       return state;
   }
